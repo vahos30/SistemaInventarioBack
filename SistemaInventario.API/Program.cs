5124dc24bb0ef04752f0e867abac6494c2861b91
vahos30 <vahos030@gmail.com>
2026-01-18 18:07:59 -0500

    se hace correcion en program en las politicas de cors

--- SistemaInventario.API/Program.cs
+++ SistemaInventario.API/Program.cs
@@ -34,11 +34,15 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("https://sistema-ventas.netlify.app",
-            "http://localhost:3000","http://72.61.70.114:3000,http://72.61.70.114:3001",
+        policy.WithOrigins(
+            "https://sistema-ventas.netlify.app",
+            "http://localhost:3000",
+            "http://72.61.70.114:3000",
+            "http://72.61.70.114:3001",
             "https://tecnofrio.jvcsoluciones.cloud")
               .AllowAnyHeader()
-              .AllowAnyMethod();
+              .AllowAnyMethod()
+              .AllowCredentials();
     });
 });
 
